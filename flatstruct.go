0069873f9d2204bceea2f43d1fdb1856107b7cc5
Philipp Sauter <sauterp@protonmail.com>
2020-05-01 16:41:27 +0200

    added func to compute nrows and ncols necessary to represent input

--- flatstruct.go
+++ flatstruct.go
@@ -8,7 +8,47 @@ import (
 /* documentation notes
 Every Go identifier has to start with a letter, thus we will use numbers for special column headers in our output.
 https://golang.org/ref/spec#identifier
+
+encode maps as JSON
 */
+
+// CompNRowsCols computes the number of rows and columns necessary to represent object s in a table.
+func CompNRowsCols(s interface{}) (nrows, ncols int) {
+	nrows = 0
+	ncols = 0
+
+	sValue := reflect.ValueOf(s)
+	switch sValue.Kind() {
+	case reflect.Slice:
+		// one table row for each slice element
+		sLen := sValue.Len()
+		nrows += sLen
+		// extra column for the element indices
+		ncols++
+
+		for i := 0; i < sLen; i++ {
+			fnrows, fncols := CompNRowsCols(sValue.Interface().([]interface{})[i])
+			nrows += fnrows
+			ncols += fncols
+		}
+
+	case reflect.Struct:
+		for i := 0; i < sValue.NumField(); i++ {
+			field := sValue.Field(i)
+
+			fnrows, fncols := CompNRowsCols(field.Interface())
+			nrows += fnrows
+			ncols += fncols
+		}
+
+	default:
+		nrows = 0
+		ncols = 1
+
+	}
+
+	return nrows, ncols
+}
 
 // Flatten TODO
 func Flatten(s interface{}) [][]string {
